9ade3f0d04e917eb6c337ad892f6315c374c0ffe
Jiří Novák <63288402+TaraJura@users.noreply.github.com>
2023-11-29 08:16:53 +0100

    Update setup-wiki.js

--- setup-wiki.js
+++ setup-wiki.js
@@ -1,15 +1,24 @@
 make sure you have node, npm
 
 
-```sudo apt install postgresql postgresql-contrib```
-```sudo systemctl start postgresql```
+sudo apt install postgresql postgresql-contrib
+sudo systemctl start postgresql
 
-```sudo -i -u postgres```
+sudo -i -u postgres
   
-```
 psql
 CREATE USER wikiuser WITH PASSWORD 'password';
 CREATE DATABASE wikidb OWNER wikiuser;
 GRANT ALL PRIVILEGES ON DATABASE wikidb TO wikiuser;
 \q
-```
+
+
+wget https://github.com/Requarks/wiki/releases/latest/download/wiki-js.tar.gz
+mkdir wiki
+tar xzf wiki-js.tar.gz -C ./wiki
+cd ./wiki
+
+npm install
+mv config.sample.yml config.yml
+nano config.yml (easy setup just read it)
+node server
